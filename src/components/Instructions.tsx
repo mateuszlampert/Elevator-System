793c2248e482161aecc66d7ec24d2177f9a89af1
mateuszlampert <mateuszlampert@gmail.com>
2024-04-01 00:09:02 +0200

    clarification instructions about making simulation step

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -3,7 +3,7 @@ import { Card, Divider, Paper, Stack, Typography } from "@mui/material";
 export default function Instructions() {
     return <>
         <Typography>
-            Hi! Welcome to the elevator manager simulator. On the left, there is a visualization of the elevators. On each floor you can order an elevator, choosing whether you plan to go up or down. Each step of the simulation causes the elevators to move one floor in the appropriate direction. "If you have already reached the elevator", you can select the floor you want to go to by clicking on the appropriate elevator in the map on the left. Above, you can peek current states of all elevators following the pattern:
+            Hi! Welcome to the elevator manager simulator. On the left, there is a visualization of the elevators. On each floor you can order an elevator, choosing whether you plan to go up or down. Each step of the simulation causes the elevators to move one floor in the appropriate direction (or wait in case of opening). "If you have already reached the elevator", you can select the floor you want to go to by clicking on the appropriate elevator in the map on the left. Above, you can peek current states of all elevators following the pattern:
         </Typography>
         <div style={{ display: "flex", justifyContent: "center" }}>
             <Paper elevation={3} sx={{ width: "40%" }}>
